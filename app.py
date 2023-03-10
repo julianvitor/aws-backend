44ba8101716b9eea303200ef7eee04045e506cec
julian <julian.vtr@gmail.com>
2023-03-10 19:13:41 -0300

    ORM and requiriments.txt

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+# -*- coding: utf-8 -*-
 from flask import Flask, request
-import boto3
+import boto3 #biblioteca AWS S3 
 import pandas as pd
-import psycopg2
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -11,18 +12,24 @@ bucket_name = '<NOME_DO_BUCKET>'
 s3_resource = boto3.resource('s3')
 bucket = s3_resource.Bucket(bucket_name)
 
-# Configuração do banco de dados
-db_host = '<ENDEREÇO>'
-db_port = '<PORTA DO BANCO DE DADOS>'
-db_name = '<NOME>'
-db_user = '<USUARIO>'
-db_pass = '<SENHA DO BANCO DE DADOS>'
+# Configuração do banco de dados (ORM)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://<USUARIO_DO_BANCO>:<SENHA_DO_BANCO>@<HOST_DO_BANCO>:<PORTA_DO_BANCO>/<NOME_DO_BANCO>'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Modelo da tabela do banco de dados
+class Tabela(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cpf = db.Column(db.String(11), nullable=False)
+    cnpj = db.Column(db.String(14), nullable=False)
+    data = db.Column(db.Date, nullable=False)
+
 
 # Função para tratar as informações do arquivo CSV
 def trata_csv(df):
     df['cpf'] = df['cpf'].str.replace('[^0-9]', '')  # remove caracteres não numéricos
     df['cnpj'] = df['cnpj'].str.replace('[^0-9]', '')
-    df['data'] = pd.to_datetime(df['data'], format='%d/%m/%Y').dt.strftime('%Y-%m-%d')  # converte a coluna data para o formato yyyy-MM-dd
+    df['data'] = pd.to_datetime(df['data'], format='%d/%m/%Y').dt.date  # converte a coluna data para o formato yyyy-MM-dd
     return df
 
 # Rota para receber os parâmetros e processar o arquivo CSV
@@ -32,10 +39,6 @@ def processar_csv():
     obj = s3.get_object(Bucket=bucket_name, Key=object_key)  # obtém o objeto do S3
     df = pd.read_csv(obj['Body'])  # CSV para dataframe
     df = trata_csv(df)  # trata as informações do dataframe
-    
-    # Conexão com o banco de dados
-    conn = psycopg2.connect(host=db_host, port=db_port, database=db_name, user=db_user, password=db_pass)
-    cursor = conn.cursor()
     
     # Insere as informações no banco de dados
     for index, row in df.iterrows():
