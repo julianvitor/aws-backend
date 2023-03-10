ce24f59481c12518e09940444eb13d4f9754b5a5
julianvitor <julian.vtr@gmail.com>
2023-03-10 16:49:14 -0300

    data processing and insert in the database

--- app.py
+++ app.py
@@ -12,16 +12,16 @@ s3_resource = boto3.resource('s3')
 bucket = s3_resource.Bucket(bucket_name)
 
 # Configuração do banco de dados
-db_host = '<HOST_DO_BANCO>'
-db_port = '<PORTA_DO_BANCO>'
-db_name = '<NOME_DO_BANCO>'
-db_user = '<USUARIO_DO_BANCO>'
-db_pass = '<SENHA_DO_BANCO>'
+db_host = '<ENDEREÇO>'
+db_port = '<PORTA DO BANCO DE DADOS>'
+db_name = '<NOME>'
+db_user = '<USUARIO>'
+db_pass = '<SENHA DO BANCO DE DADOS>'
 
 # Função para tratar as informações do arquivo CSV
 def trata_csv(df):
-    df['cpf'] = df['cpf'].str.replace('[^0-9]', '')  # remove caracteres não numéricos do cpf
-    df['cnpj'] = df['cnpj'].str.replace('[^0-9]', '')  # remove caracteres não numéricos do cnpj
+    df['cpf'] = df['cpf'].str.replace('[^0-9]', '')  # remove caracteres não numéricos
+    df['cnpj'] = df['cnpj'].str.replace('[^0-9]', '')
     df['data'] = pd.to_datetime(df['data'], format='%d/%m/%Y').dt.strftime('%Y-%m-%d')  # converte a coluna data para o formato yyyy-MM-dd
     return df
 
@@ -30,18 +30,20 @@ def trata_csv(df):
 def processar_csv():
     object_key = request.form['object_key']  # obtém o nome do arquivo no S3
     obj = s3.get_object(Bucket=bucket_name, Key=object_key)  # obtém o objeto do S3
-    df = pd.read_csv(obj['Body'])  # carrega o arquivo CSV em um dataframe
+    df = pd.read_csv(obj['Body'])  # CSV para dataframe
     df = trata_csv(df)  # trata as informações do dataframe
+    
     # Conexão com o banco de dados
     conn = psycopg2.connect(host=db_host, port=db_port, database=db_name, user=db_user, password=db_pass)
     cursor = conn.cursor()
+    
     # Insere as informações no banco de dados
     for index, row in df.iterrows():
         cursor.execute("INSERT INTO tabela (cpf, cnpj, data) VALUES (%s, %s, %s)", (row['cpf'], row['cnpj'], row['data']))
     conn.commit()
     cursor.close()
     conn.close()
-    return 'Arquivo CSV processado com sucesso!'
+    return 'Arquivo CSV processado!'
 
 if __name__ == '__main__':
     app.run(debug=True)
